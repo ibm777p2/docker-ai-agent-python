d3cecbcebb662f7aaa16d840792981736803af71
ibm777p2 <ibm777p2@gmail.com>
2026-09-14 13:54:09 -0700

    Fixed DATABASE URL

--- backend/src/api/db.py
+++ backend/src/api/db.py
@@ -6,7 +6,10 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL == "":
     raise NotImplementedError("No database implemented, `DATABASE_URL` needs to be set.")
 
-DATABASE_URL = DATABASE_URL.replace("postgres://", "postgres+psycopg://")
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+elif DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
 
 engine = sqlmodel.create_engine(DATABASE_URL)
 #database models
