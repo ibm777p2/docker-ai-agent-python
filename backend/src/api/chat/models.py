85af0dc8327bb43354f354688b7685a970c7ca62
ibm777p2 <ibm777p2@gmail.com>
2026-09-14 13:17:43 -0700

    Updated the datbase for models

--- backend/src/api/chat/models.py
+++ backend/src/api/chat/models.py
@@ -1,5 +1,8 @@
-from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field, DateTime
 
+def get_utc_now():
+    return datetime.now().replace(tzinfo=timezone.utc)
 class ChatMessagePayload(SQLModel):
     # pydantic model for validation
     # validation
@@ -12,3 +15,14 @@ class ChatMessage(SQLModel, table=True):
     # serializer
     id: int | None = Field(default=None, primary_key=True)
     message: str
+    created_at: datetime = Field(
+        default_factory = get_utc_now,
+        sa_type=DateTime(timezone = True), # sqlalchemey, timescaledb 
+        primary_key = False,
+        nullable = False,
+    )
+    
+class ChatMessageListItem(SQLModel):
+    id: int| None = Field(default=None)
+    message: str
+    created_at: datetime = Field(default=None)
--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,6 +1,7 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
-from .models import ChatMessagePayload, ChatMessage
+from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 from api.db import get_session
 router = APIRouter()
 
@@ -11,7 +12,7 @@ def chat_health():
 
 # /api/chats/recent/
 # curl http://localhost:8080/api/chats/recent/
-@router.get("/recent/")
+@router.get("/recent/", response_model = List[ChatMessageListItem])
 def chat_list_messages(session: Session= Depends(get_session)):
     query = select(ChatMessage)# sql -> query
     results = session.exec(query).fetchall()[:10]
@@ -19,7 +20,7 @@ def chat_list_messages(session: Session= Depends(get_session)):
 
 # HTTP POST -> {"message": "Nice site"} -> {"message": "Nice site", "id": 1}
 # curl -X POST -d '{"message": "Nice site"}' -H "Content-type: application/json" http://localhost:8080/api/chats/
-@router.post("/", response_model=ChatMessage)
+@router.post("/", response_model=ChatMessageListItem)
 def chat_create_message(
     payload:ChatMessagePayload,
     session: Session = Depends(get_session)
